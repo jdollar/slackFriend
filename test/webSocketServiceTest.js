1fe565aeeccdeaba4119f296c1f37a785a950df5
jdollar <dollarhidejoel@gmail.com>
2016-03-26 09:04:41 -0400

    Added a couple more tests for the webSocketService. Cleaned up package.json

--- test/webSocketServiceTest.js
+++ test/webSocketServiceTest.js
@@ -2,23 +2,14 @@
 	'use strict'
 	var Nook = require('nock'),
 			Sinon = require('sinon'),
-			Mockery = require('mockery'),
 			assert = require('assert')
 
 	var webSocketService
 	
 	describe('webSocketServiceTest', function() {
 	
-		before(function() {
-			//Mockery.enable()
-		})
-	
 		beforeEach(function() {
 			webSocketService = require('../lib/webSocketService')('https://www.test.com')
-		})
-	
-		after(function() {
-			//Mockery.disable()
 		})
 	
 		describe('open', function() {
@@ -33,6 +24,44 @@
 			})
 		})
 
+		describe('sendSimpleMessage', function() {
+
+			before(function() {
+			})
+
+			after(function() {
+			})
+			
+			it('send message proper values', function() {
+				var spy = Sinon.stub(webSocketService, 'send', function(jsonString) {})
+				var expectedChannel = 'channel'
+				var expectedMessage = 'message'
+				var expectedJsonString = '{"id": 1, "type": "message", "channel": "' + expectedChannel + '", "text": "' + expectedMessage + '"}'
+
+			 	webSocketService.sendSimpleMessage(expectedChannel, expectedMessage)	
+				assert(spy.withArgs(expectedJsonString).calledOnce)
+			})
+
+			it('send message blank values', function() {
+				var spy = Sinon.stub(webSocketService, 'send', function(jsonString) {})
+				var expectedChannel = ''
+				var expectedMessage = ''
+				var expectedJsonString = '{"id": 1, "type": "message", "channel": "' + expectedChannel + '", "text": "' + expectedMessage + '"}'
+
+			 	webSocketService.sendSimpleMessage(expectedChannel, expectedMessage)	
+				assert(spy.withArgs(expectedJsonString).calledOnce)
+			})
+
+			it('send message undefined values', function() {
+				var spy = Sinon.stub(webSocketService, 'send', function(jsonString) {})
+				var expectedChannel
+				var expectedMessage
+				var expectedJsonString = '{"id": 1, "type": "message", "channel": "' + expectedChannel + '", "text": "' + expectedMessage + '"}'
+
+			 	webSocketService.sendSimpleMessage(expectedChannel, expectedMessage)	
+				assert(spy.withArgs(expectedJsonString).calledOnce)
+			})
+		})
 
 		function shouldLogMessage(functionCall, message) {
 			var oldLog = console.log
