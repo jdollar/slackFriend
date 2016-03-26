1443b2f069c1006c5a8de788fb3ff55832f4ce5e
jdollar <dollarhidejoel@gmail.com>
2016-03-26 02:47:16 -0400

    Added ws a websocket library. Allows a basic connection and message sent to the slack channel

--- src/slackFriend.js
+++ src/slackFriend.js
@@ -2,8 +2,9 @@
 	var $https = require('https')
 	var fs = require('fs')
 	var config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json', 'utf-8'))
+	var messagePosted = false
 
-	$https.get('https://' + config.teamName + '.slack.com/api/rtm.start?token=' + config.token, function(res) {
+	$https.get('https://' + config.teamName + '.slack.com/api/rtm.start?token=' + config.token, (res) => {
 		var data = ''
 
 		res.on('data', function(dataChunk) {
@@ -12,7 +13,29 @@
 
 		res.on('end', function() {
 			var rtmStartObject = JSON.parse(data)
-			console.log(rtmStartObject)
+			var channels = rtmStartObject.channels
+
+			var WebSocket = require('ws')
+			var ws = new WebSocket(rtmStartObject.url)
+
+			ws.on('open', function() {
+				console.log('connected')
+			})
+
+			ws.on('close', function() {
+				console.log('disconnected')
+			})
+
+			ws.on('message', function(data, flags) {
+				console.log(data)
+				for (var channel of channels) {
+					if (channel.name === 'general' && messagePosted == false) {
+						ws.send('{"id": 1, "type": "message", "channel": "' + channel.id + '", "text": "test"}')
+						messagePosted = true
+					}
+				}
+			})
 		})
 	})
+
 })()
