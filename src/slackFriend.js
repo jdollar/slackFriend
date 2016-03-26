4522b87773ccc028e6a20951d0b29da7a382f985
jdollar <dollarhidejoel@gmail.com>
2016-03-25 23:36:11 -0400

    Added teamname for url

--- src/slackFriend.js
+++ src/slackFriend.js
@@ -2,4 +2,5 @@ var $https = require('https')
 var fs = require('fs')
 var config = fs.readFileSync(__dirname + '/config/config.json')
 
-$https.get("https://drunkcraft.slack.com/api/rtm.start?token=" + config.token, function(res) {console.log(res)})
+$https.get('https://' + config.teamName + '.slack.com/api/rtm.start?token=' + config.token, function(res) {
+	console.log(res)})
